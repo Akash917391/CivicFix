a5d11bfa34600702ee6f44676890585326d4f93a
Akash917391 <akashboricha09@gmail.com>
2026-05-29 01:22:52 +0530

    Added templates and static folders

--- CivicFix/settings.py
+++ CivicFix/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "CivicFix.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR.parent / 'templates'],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
 
-    BASE_DIR.parent / "static",
+    BASE_DIR / "static",
 
 ]
 
